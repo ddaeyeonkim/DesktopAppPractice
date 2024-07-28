f594c758e6978ce49c87b8637d558f3aa93b69cc
ddaeyeonkim <improve777@gmail.com>
2024-07-29 00:30:57 +0900

    DialogWindow 구현

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,9 +1,13 @@
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.window.DialogWindow
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberDialogState
 
 val commandExecutor = CommandExecutor()
 
@@ -12,7 +16,23 @@ fun main() = application {
         onCloseRequest = ::exitApplication,
         title = "DesktopAppPractice",
     ) {
+        var isDialogOpen by remember { mutableStateOf(false) }
+
         var commandResult by remember { mutableStateOf("") }
+
+        Button(onClick = { isDialogOpen = true }) {
+            Text("Open Dialog")
+        }
+
+        if (isDialogOpen) {
+            // TODO: 생각했던 dialog가 아닌데..
+            DialogWindow(
+                onCloseRequest = { isDialogOpen = false },
+                state = rememberDialogState()
+            ) {
+                Text("Dialog Content")
+            }
+        }
 
         App(
             commandResult = commandResult,
