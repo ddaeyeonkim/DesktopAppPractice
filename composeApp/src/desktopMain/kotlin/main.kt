ef24da50e75f7d3f5ff63fee0bff14e042c4f868
ddaeyeonkim <improve777@gmail.com>
2024-07-29 00:39:59 +0900

    singleWindowApplication 적용

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -5,57 +5,52 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.DialogWindow
-import androidx.compose.ui.window.Tray
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberDialogState
-import desktopapppractice.composeapp.generated.resources.Res
-import desktopapppractice.composeapp.generated.resources.compose_multiplatform
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.ui.window.singleWindowApplication
 
 val commandExecutor = CommandExecutor()
 
-fun main() = application {
+fun main() = singleWindowApplication {
     var isVisible by remember { mutableStateOf(true) }
-    Window(
-        onCloseRequest = { isVisible = false },
-        visible = isVisible,
-        title = "DesktopAppPractice",
-    ) {
-        var isDialogOpen by remember { mutableStateOf(false) }
+//    Window(
+//        onCloseRequest = { isVisible = false },
+//        visible = isVisible,
+//        title = "DesktopAppPractice",
+//    ) {
+    var isDialogOpen by remember { mutableStateOf(false) }
 
-        var commandResult by remember { mutableStateOf("") }
+    var commandResult by remember { mutableStateOf("") }
 
-        Button(onClick = { isDialogOpen = true }) {
-            Text("Open Dialog")
-        }
-
-        if (isDialogOpen) {
-            // TODO: 생각했던 dialog가 아닌데..
-            DialogWindow(
-                onCloseRequest = { isDialogOpen = false },
-                state = rememberDialogState()
-            ) {
-                Text("Dialog Content")
-            }
-        }
-
-        if (!isVisible) {
-            Tray(
-                painterResource(Res.drawable.compose_multiplatform),
-                tooltip = "DesktopAppPractice",
-                onAction = { isVisible = true },
-                menu = {
-                    Item("Exit", onClick = ::exitApplication)
-                }
-            )
-        }
-
-        App(
-            commandResult = commandResult,
-            onCommandRunClick = {
-                commandResult = commandExecutor.execute("ls")
-            }
-        )
+    Button(onClick = { isDialogOpen = true }) {
+        Text("Open Dialog")
     }
+
+    if (isDialogOpen) {
+        // TODO: 생각했던 dialog가 아닌데..
+        DialogWindow(
+            onCloseRequest = { isDialogOpen = false },
+            state = rememberDialogState()
+        ) {
+            Text("Dialog Content")
+        }
+    }
+
+    if (!isVisible) {
+//            Tray(
+//                painterResource(Res.drawable.compose_multiplatform),
+//                tooltip = "DesktopAppPractice",
+//                onAction = { isVisible = true },
+//                menu = {
+//                    Item("Exit", onClick = ::exitApplication)
+//                }
+//            )
+    }
+
+    App(
+        commandResult = commandResult,
+        onCommandRunClick = {
+            commandResult = commandExecutor.execute("ls")
+        }
+    )
+//    }
 }
